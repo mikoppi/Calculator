473937b6c54b77f7777d5015ececdfeb22d88bb8
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-02-25 13:33:14 +0200

    Display content and result

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ const divide = function (a,b) {
     return a/b;
 };
 
-// Function that takes an operator and 2 numbers and call
+// Function that takes an operator and 2 numbers and calls
 // one of the above functions
 
-function operate (operator,num1,num2) {
+function operate (num1,operator,num2) {
     let numberOne=Number(num1);
     let numberTwo=Number(num2);
     let operators=['+','-','*','/'];
@@ -37,15 +37,43 @@ function operate (operator,num1,num2) {
 
 //functions that make pressed numbers/operators appear on display div
 
-const display=document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
+let contentArray=[];
+let tempResult=0;
+const displayContent=document.querySelector('.user-input');
+const buttons = document.querySelectorAll('.excludeEqual');
 buttons.forEach((button)=> {
     button.addEventListener('click',showTextContent)
 })
 
+const displayResult=document.querySelector('.result');
+const resultButton=document.getElementById('=');
+resultButton.addEventListener('click', showResult);
+
+
 function showTextContent (e) {
     let text=(e.target.id);
-    display.textContent=text;
-    
+    if (text!=='delete'&&text!=='clear'&&text!=='=') {
+        contentArray.push(text);
+    }
+    else if (text=='delete') {
+        contentArray.pop();
+    }
+    else if (text=='clear') {
+        contentArray.length=0;
+    }
+    displayContent.textContent=contentArray.join('');
+    return contentArray;
 }
 
+
+function showResult (e) {
+    while (contentArray.length!=1) {
+        let result=operate(contentArray[0],contentArray[1],contentArray[2]);
+        contentArray.splice(0,3,result);
+    }
+        
+    displayResult.textContent=result;
+    }
+
+
+   
